3e8caf16f6974f913f44d43bafe836782562fa5f
Dan Rapp <rappdw@gmail.com>
2017-03-19 20:43:33 -0600

    Cleanup

--- redblackgraph/redblackgraph.py
+++ redblackgraph/redblackgraph.py
@@ -23,26 +23,6 @@ def avos(a, b):
     return (b & (pow(2, generationNumber) - 1)) | (a << generationNumber)
 
 
-def _csr_matmat_pass1(n_row, n_col, Ap, Aj, Bp, Bj, Cp):
-    mask = np.empty(n_col, dtype=np.int32)
-    mask.fill(-1)
-    Cp[0] = 0
-    nnz = 0
-
-    for i in range(0, n_row):
-        row_nnz = 0
-        for jj in range(Ap[i], Ap[i + 1]):
-            j = Aj[jj]
-            for kk in range(Bp[j], Bp[j + 1]):
-                k = Bj[kk]
-                if mask[k] != i:
-                    mask[k] = i
-                    row_nnz += 1
-        next_nnz = nnz + row_nnz
-        nnz = next_nnz
-        Cp[i + 1] = nnz
-    pass
-
 def accumulate_cell_value(cell, transitive_relationship):
     return transitive_relationship if cell == 0 else min(cell, transitive_relationship)
 
@@ -97,7 +77,6 @@ def expand(m):
     indptr = np.empty(major_axis + 1, dtype=idx_dtype)
 
     fn = getattr(_sparsetools, m.format + '_matmat_pass1')
-    #fn = _csr_matmat_pass1
 
     fn(M, N,
        np.asarray(m.indptr, dtype=idx_dtype),
