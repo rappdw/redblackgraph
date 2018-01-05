990751005f16c2c3639100cfa7d82c5be15737f2
Dan Rapp <rappdw@gmail.com>
2018-01-04 21:18:30 -0700

    Fix test case Add a more complicated edge_relational_composition test case

--- redblackgraph/tests/simple/test_rel_composition.py
+++ redblackgraph/tests/simple/test_rel_composition.py
@@ -1,5 +1,5 @@
 from redblackgraph.simple import vertex_relational_composition, edge_relational_composition
-
+from redblackgraph.simple import warshall
 
 def test_vertex_relational_composition():
     # use the A+ from the example in our notebook, add in a sibling to the last vertex in the graph
@@ -30,41 +30,42 @@ def test_vertex_relational_composition():
                         [2, 4, 5, 8, 0, -1, 9],
                         [0, 0, 0, 0, 0, 0, 1]]
 
-def test_my_use_case():
-    #       D   E   R   M   H  Mi   A   I  Do  Ev   G  Ma   S  Em
-    A1 = [[ -1,  2,  3, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],  # D
-          [  0, -1,  0, 0,  0,  2,  3,  0,  0,  0,  0,  0,  0,  0],  # E
-          [  0,  0,  1, 0,  0,  0,  0,  3,  0,  0,  0,  0,  0,  0],  # R
-          [  0,  0,  0, 1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],  # M
-          [  0,  2,  0, 3, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0],  # H
-          [  0,  0,  0, 0,  0, -1,  0,  0,  0,  0,  2,  3,  0,  0],  # Mi
-          [  0,  0,  0, 0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0],  # A
-          [  0,  0,  0, 0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0],  # I
-          [  0,  0,  0, 0,  0,  0,  0,  0, -1,  3,  0,  0,  0,  0],  # Do
-          [  0,  0,  0, 0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0],  # Ev
-          [  0,  0,  0, 0,  0,  0,  0,  0,  0,  0, -1,  0,  0,  0],  # G
-          [  0,  0,  0, 0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0],  # Ma
-          [  0,  0,  0, 0,  0,  0,  0,  0,  0,  0,  0,  0, -1,  0],  # S
-          [  0,  0,  0, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1]  # Em
+def test_my_use_case_vertex():
+    #        D   E   R   M   H  Mi   A   I  Do  Ev   G  Ma   S  Em
+    A1 = [[ -1,  2,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],  # D
+          [  0, -1,  0,  0,  0,  2,  3,  0,  0,  0,  0,  0,  0,  0],  # E
+          [  0,  0,  1,  0,  0,  0,  0,  3,  0,  0,  0,  0,  0,  0],  # R
+          [  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],  # M
+          [  0,  2,  0,  3, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0],  # H
+          [  0,  0,  0,  0,  0, -1,  0,  0,  0,  0,  2,  3,  0,  0],  # Mi
+          [  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0],  # A
+          [  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0],  # I
+          [  0,  0,  0,  0,  0,  0,  0,  0, -1,  3,  0,  0,  0,  0],  # Do
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0],  # Ev
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1,  0,  0,  0],  # G
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0],  # Ma
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1,  0],  # S
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1]  # Em
          ]
     #      D   E   R   M   H  Mi   A   I  Do  Ev   G  Ma   S  Em   J
     u = [[ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2,  3]]
     v = [[ 0], # D
-     [ 0], # E
-     [ 2], # R
-     [ 0], # M
-     [ 0], # H
-     [ 0], # Mi
-     [ 0], # A
-     [ 0], # I
-     [ 2], # Do
-     [ 0], # Ev
-     [ 0], # G
-     [ 0], # Ma
-     [ 0], # S
-     [ 0], # Em
-     ]
-    A_lambda = vertex_relational_composition(u, A1, v, -1)
+         [ 0], # E
+         [ 2], # R
+         [ 0], # M
+         [ 0], # H
+         [ 0], # Mi
+         [ 0], # A
+         [ 0], # I
+         [ 2], # Do
+         [ 0], # Ev
+         [ 0], # G
+         [ 0], # Ma
+         [ 0], # S
+         [ 0], # Em
+         ]
+    R = warshall(A1)[0].tolist()
+    A_lambda = vertex_relational_composition(u, R, v, -1)
     assert A_lambda[0][12] == 12
     assert A_lambda[0][13] == 13
     assert A_lambda[0][14] == 6
@@ -74,8 +75,10 @@ def test_my_use_case():
     assert A_lambda[8][12] == 4
     assert A_lambda[8][13] == 5
     assert A_lambda[8][14] == 2
+    # print()
+    # print(np.array(A_lambda))
 
-def test_edge_relational_composition():
+def test_edge_relational_composition_simple():
     R = [[-1, 0, 3, 0, 0],
          [ 0,-1, 0, 2, 0],
          [ 0, 0, 1, 0, 0],
@@ -88,3 +91,34 @@ def test_edge_relational_composition():
                   [ 0, 0, 0,-1, 0],
                   [ 2, 4, 5, 8, 1]]
     assert R_lambda == R_expected
+
+def test_my_use_case_edge():
+    #        D   E   R   M   H  Mi   A   I  Do  Ev   G  Ma   S  Em   J
+    A1 = [[ -1,  2,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],  # D
+          [  0, -1,  0,  0,  0,  2,  3,  0,  0,  0,  0,  0,  0,  0,  0],  # E
+          [  0,  0,  1,  0,  0,  0,  0,  3,  0,  0,  0,  0,  0,  0,  0],  # R
+          [  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],  # M
+          [  0,  2,  0,  3, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],  # H
+          [  0,  0,  0,  0,  0, -1,  0,  0,  0,  0,  2,  3,  0,  0,  0],  # Mi
+          [  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0],  # A
+          [  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0],  # I
+          [  0,  0,  0,  0,  0,  0,  0,  0, -1,  3,  0,  0,  0,  0,  2],  # Do
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0],  # Ev
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0],  # G
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0],  # Ma
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1,  0,  0],  # S
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0], # Em
+          [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2,  3, -1]  # J
+          ]
+    R = warshall(A1)[0]
+    # Missing edge is R -> J, 2
+    A_lambda = edge_relational_composition(R, 2, 14, 2)
+    assert A_lambda[0][12] == 12
+    assert A_lambda[0][13] == 13
+    assert A_lambda[0][14] == 6
+    assert A_lambda[2][12] == 4
+    assert A_lambda[2][13] == 5
+    assert A_lambda[2][14] == 2
+    assert A_lambda[8][12] == 4
+    assert A_lambda[8][13] == 5
+    assert A_lambda[8][14] == 2
