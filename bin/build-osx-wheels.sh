3ac485311176b33bba4e5191aa325d2a87f595a4
Daniel Rapp <rappdw@gmail.com>
2020-03-25 22:10:45 -0600

    Add cython to wheel building scripts

--- bin/build-osx-wheels.sh
+++ bin/build-osx-wheels.sh
@@ -3,17 +3,17 @@
 eval "$(conda shell.bash hook)"
 
 conda activate rgb-build-36
-pip install -U numpy
+pip install -U numpy cython
 python setup.py bdist_wheel -d ./wheel
 conda deactivate
 
 conda activate rgb-build-37
-pip install -U numpy
+pip install -U numpy cython
 python setup.py bdist_wheel -d ./wheel
 conda deactivate
 
 conda activate rgb-build-38
-pip install -U numpy
+pip install -U numpy cython
 python setup.py bdist_wheel -d ./wheel
 conda deactivate
 
