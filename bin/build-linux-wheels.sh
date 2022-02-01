95fe1df525b4313a3836652a39cb43bc8f621b94
Dan Rapp <rappdw@gmail.com>
2022-01-31 17:36:22 -0700

    Update python version supported

--- bin/build-linux-wheels.sh
+++ bin/build-linux-wheels.sh
@@ -15,3 +15,13 @@ rm /tmp/wheel/*
 /opt/python/cp38-cp38/bin/pip wheel /workdir -w /tmp/wheel
 auditwheel repair /tmp/wheel/RedBlackGraph*.whl -w /workdir/wheel
 rm /tmp/wheel/*
+
+/opt/python/cp39-cp39/bin/pip install numpy cython
+/opt/python/cp39-cp39/bin/pip wheel /workdir -w /tmp/wheel
+auditwheel repair /tmp/wheel/RedBlackGraph*.whl -w /workdir/wheel
+rm /tmp/wheel/*
+
+/opt/python/cp310-cp310/bin/pip install numpy cython
+/opt/python/cp310-cp310/bin/pip wheel /workdir -w /tmp/wheel
+auditwheel repair /tmp/wheel/RedBlackGraph*.whl -w /workdir/wheel
+rm /tmp/wheel/*
