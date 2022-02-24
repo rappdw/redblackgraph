7bfae019dcbedde2abf8cfc6f1ba64e2b32ceb78
Dan Rapp <rappdw@gmail.com>
2022-02-24 14:27:27 -0700

    Build manylinux2014 wheels

--- bin/build-wheels.sh
+++ bin/build-wheels.sh
@@ -7,3 +7,4 @@ mkdir -p ./wheel
 bin/build-osx-wheels.sh
 
 docker run --rm -it --mount type=bind,source=$(pwd),target=/workdir --entrypoint=/workdir/bin/build-linux-wheels.sh quay.io/pypa/manylinux_2_24_x86_64
+docker run --rm -it --mount type=bind,source=$(pwd),target=/workdir --entrypoint=/workdir/bin/build-linux-wheels.sh quay.io/pypa/manylinux2014_x86_64
